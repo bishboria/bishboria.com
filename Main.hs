3c9996e7f47db35a9ab7cabe7936dd42f7401644
Stuart Gale <bishboria@gmail.com>
2014-07-22 14:55:14 +0100

    Add new  route

--- Main.hs
+++ Main.hs
@@ -6,6 +6,11 @@ import Web.Scotty
 main :: IO ()
 main = do
     port <- getEnv "PORT"
-    scotty (read port) $
-      get "/" $
-        file "index.html"
+
+    scotty (read port) $ do
+        get "/" $ do
+            file "index.html"
+
+        get "/posts/:post" $ do
+            post <- param "post"
+            file (post ++ ".html")
